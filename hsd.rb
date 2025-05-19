e0b785827c8c75dd9c2fc0b801395e7444c4ce83
JB <jamboyki@cisco.com>
2025-05-19 09:10:48 -0400

    Update hsd formula for 1.7

--- hsd.rb
+++ hsd.rb
@@ -1,14 +1,14 @@
 class Hsd < Formula
   desc "hsd"
-  version "1.5"
+  version "1.7"
 
   on_arm do
-    url "https://github.com/jb-cisco/homebrew-hsd/releases/download/v1.5/hsd-arm64"
+    url "https://github.com/jb-cisco/homebrew-hsd/releases/download/v1.7/hsd-arm64"
     sha256 "7cf121fe6a0e420a79ed42954533051899381c5c3c677919161f9a62675bf32e"
   end
 
   on_intel do
-    url "https://github.com/jb-cisco/homebrew-hsd/releases/download/v1.5/hsd-amd64"
+    url "https://github.com/jb-cisco/homebrew-hsd/releases/download/v1.7/hsd-amd64"
     sha256 "dcffc22f8db030b8c9e37ab12c15b8ee994be0ab02ad142b02c9eb1e0f998c9e"
   end
 
@@ -24,14 +24,14 @@ class Hsd < Formula
     end
 
     recommended = ["awscli", "kubernetes-cli", "helm"]
-    null_device = defined?(File::NULL) ? File::NULL : "/dev/null"
     missing = recommended.reject do |pkg|
-      system("brew", "list", pkg, out: null_device, err: null_device)
+      quiet_system "brew", "list", "--formula", pkg
     end
+    
     unless missing.empty?
       ohai "Recommended Homebrew packages missing: #{missing.join(', ')}"
       ohai "You can install them with:"
-      puts "  brew install #{missing.join(' ')}"
+      ohai "  brew install #{missing.join(' ')}"
     end
   end
 end
